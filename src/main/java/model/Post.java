c9fbb13c36d46f80133eeea6f743280913da718f
ogneyar <maksimus.s@mail.ru>
2020-07-29 12:14:47 +0300

    SrcChange

---
+++ src/main/java/model/Post.java
@@ -0,0 +1,43 @@
+package model;
+
+import java.util.Objects;
+
+public class Post {
+
+    private int id;
+    private String name;
+
+    public Post(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return id == post.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}

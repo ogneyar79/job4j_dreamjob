afde4d56006fb86f99ec52b17b4b92302f700214
ogneyar <maksimus.s@mail.ru>
2020-07-30 16:45:00 +0300

    StoreAddCandidate

--- src/main/java/store/Store.java
+++ src/main/java/store/Store.java
@@ -1,5 +1,6 @@
 package store;
 
+import model.Candidate;
 import model.Post;
 
 import java.util.Collection;
@@ -11,18 +12,27 @@ public class Store {
     private static final Store INST = new Store();
 
     private Map<Integer, Post> posts = new ConcurrentHashMap<>();
+    private Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
+
 
     private Store() {
         posts.put(1, new Post(1, "Junior Java Job"));
         posts.put(2, new Post(2, "Middle Java Job"));
         posts.put(3, new Post(3, "Senior Java Job"));
+        candidates.put(1, new Candidate(1, "Junior Java"));
+        candidates.put(2, new Candidate(2, "Middle Java"));
+        candidates.put(3, new Candidate(3, "Senior Java"));
     }
 
     public static Store instOf() {
         return INST;
     }
 
-    public Collection<Post> findAll() {
+    public Collection<Post> findAllPosts() {
         return posts.values();
+    }
+
+    public Collection<Candidate> findAllCandidates() {
+        return candidates.values();
     }
 }

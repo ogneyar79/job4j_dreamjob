11e314caab518a0002f156729506fd3c65d0514b
ogneyar <maksimus.s@mail.ru>
2020-08-04 10:02:30 +0300

    StoreAddSaveCandidate

--- src/main/java/store/Store.java
+++ src/main/java/store/Store.java
@@ -12,6 +12,7 @@ public class Store {
 
     private static final Store INST = new Store();
     private static AtomicInteger POST_ID = new AtomicInteger(4);
+    private static AtomicInteger CANDIDATE_ID = new AtomicInteger(3);
 
     private Map<Integer, Post> posts = new ConcurrentHashMap<>();
     private Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
@@ -29,6 +30,11 @@ public class Store {
     public void save(Post post) {
         post.setId(POST_ID.incrementAndGet());
         posts.put(post.getId(), post);
+    }
+
+    public void save(Candidate candidate) {
+        candidate.setId(CANDIDATE_ID.incrementAndGet());
+        candidates.put(candidate.getId(), candidate);
     }
 
     public Collection<Post> findAll() {

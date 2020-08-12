f3646e584724399bff8adbe49ab898e94c3f33a8
ogneyar79 <maksimus.s@mail.ru>
2020-08-12 09:41:51 +0300

    PsqlStoreCreated

---
+++ src/main/java/store/PsqlStore.java
@@ -0,0 +1,138 @@
+package store;
+
+import model.Candidate;
+import model.Post;
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Properties;
+
+public class PsqlStore implements IStore {
+
+    private final BasicDataSource pool = new BasicDataSource();
+
+    private PsqlStore() {
+        Properties cfg = new Properties();
+        try (BufferedReader io = new BufferedReader(
+                new FileReader("db.properties")
+        )) {
+            cfg.load(io);
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+        try {
+            Class.forName(cfg.getProperty("jdbc.driver"));
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+        pool.setDriverClassName(cfg.getProperty("jdbc.driver"));
+        pool.setUrl(cfg.getProperty("jdbc.url"));
+        pool.setUsername(cfg.getProperty("jdbc.username"));
+        pool.setPassword(cfg.getProperty("jdbc.password"));
+        pool.setMinIdle(5);
+        pool.setMaxIdle(10);
+        pool.setMaxOpenPreparedStatements(100);
+    }
+
+    private static final class Lazy {
+        private static final IStore INST = new PsqlStore();
+    }
+
+    public static IStore instOf() {
+        return Lazy.INST;
+    }
+
+
+    @Override
+    public Collection<Post> findAllPosts() {
+        List<Post> posts = new ArrayList<>();
+        try (Connection cn = pool.getConnection();
+             PreparedStatement ps = cn.prepareStatement("SELECT * FROM post")
+        ) {
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return posts;
+    }
+
+    @Override
+    public Collection<Candidate> findAllCandidates() {
+        List<Candidate> candidates = new ArrayList<>();
+        try (Connection cn = pool.getConnection();
+             PreparedStatement ps = cn.prepareStatement("SELECT * FROM candidate")
+        ) {
+            try (ResultSet it = ps.executeQuery()) {
+                while (it.next()) {
+                    candidates.add(new Candidate(it.getInt("id"), it.getString("name")));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return candidates;
+    }
+
+    @Override
+    public void save(Post post) {
+        if (post.getId() == 0) {
+            create(post);
+        } else {
+            update(post);
+        }
+    }
+
+    private Post create(Post post) {
+        try (Connection cn = pool.getConnection();
+             PreparedStatement ps = cn.prepareStatement("INSERT INTO post(name) VALUES (?)", PreparedStatement.RETURN_GENERATED_KEYS)
+        ) {
+            ps.setString(1, post.getName());
+            ps.execute();
+            try (ResultSet id = ps.getGeneratedKeys()) {
+                if (id.next()) {
+                    post.setId(id.getInt(1));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return post;
+    }
+
+    private void update(Post post) {
+        try (Connection cn = pool.getConnection();
+             PreparedStatement ps = cn.prepareStatement(" UPDATE post set name = ? where id = ? ")
+        ) {
+            ps.setString(1, post.getName());
+            ps.setString(2, String.valueOf(post.getId()));
+            ps.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    @Override
+    public Post findById(int id) {
+        Post post = new Post(0, "");
+        try (Connection cn = pool.getConnection();
+             PreparedStatement ps = cn.prepareStatement("SELECT from post where id = ?")
+        ) {
+            ps.setInt(1, id);
+            try (ResultSet resultSet = ps.executeQuery()) {
+                if (resultSet.next()) {
+                    post = new Post(resultSet.getInt("id"), resultSet.getString("name"));
+                }
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return post;
+    }
+}

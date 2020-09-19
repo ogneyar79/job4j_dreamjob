d1430a33e41ac80bc9dbda71d1aa89f115707bcc
ogneyar79 <maksimus.s@mail.ru>
2020-09-19 13:37:59 +0300

    DownloadServletMade

--- src/main/java/servlet/DownloadServlet.java
+++ src/main/java/servlet/DownloadServlet.java
@@ -1,5 +1,9 @@
 package servlet;
 
+import model.Photo;
+import store.IStore;
+import store.PsqlStore;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,7 +16,16 @@ public class DownloadServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String name = req.getParameter("name");
+
+        int photoId = Integer.parseInt(req.getParameter("photoId"));
+        if (photoId == 0) {
+            String path = req.getContextPath() + "/notfound";
+            resp.sendRedirect(path);
+            return;
+        }
+        IStore store = PsqlStore.instOf();
+        Photo photo = store.findPhotoById(photoId);
+        String name = photo.getName();
         resp.setContentType("name=" + name);
         resp.setContentType("image/png");
         resp.setHeader("Content-Disposition", "attachment; filename=\"" + name + "\"");

23258da9f9b93ce435ccdc6f6a604e495784f13c
ogneyar79 <maksimus.s@mail.ru>
2020-10-11 20:51:06 +0300

    DownloadServletCorrectAllBetter

--- src/main/java/servlet/DownloadServlet.java
+++ src/main/java/servlet/DownloadServlet.java
@@ -1,7 +1,7 @@
 package servlet;
 
 import model.Photo;
-import servlet.methodfordownloading.ServletResponseOutfile;
+
 
 import store.PsqlStore;
 
@@ -14,24 +14,23 @@ import java.io.IOException;
 
 
 public class DownloadServlet extends HttpServlet {
-
+    private void showImage(String name, HttpServletResponse resp) throws IOException {
+        resp.setContentType("image/png");
+        resp.setHeader("Content-Disposition", "attachment; filename=\"" + name + "\"");
+        File file = new File("images" + File.separator + name);
+        try (FileInputStream in = new FileInputStream(file)) {
+            resp.getOutputStream().write(in.readAllBytes());
+        }
+    }
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         System.out.println(" Do Get DS");
         int photoId = Integer.parseInt(req.getParameter("photo"));
         Photo photo = PsqlStore.instOf().findPhotoById(photoId);
         String name = photo.getName();
-        if (photoId != 0) {
-            resp.setContentType("image/png");
-            resp.setHeader("Content-Disposition", "attachment; filename=\"" + name + "\"");
-            File file = new File("images" + File.separator + name);
-            try (FileInputStream in = new FileInputStream(file)) {
-                resp.getOutputStream().write(in.readAllBytes());
-            }
-        } else {
-            try (FileInputStream in = new FileInputStream(new File("C:\\Tools\\apache-tomcat-9.0.37\\bin\\images\\Screenshot_1.png"))) {
-                resp.getOutputStream().write(in.readAllBytes());
-            }
+        if (photoId == 0) {
+            name = "Screenshot_1.png";
         }
+        showImage(name, resp);
     }
 }

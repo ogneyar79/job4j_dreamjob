44e49cf3aa8f585dbaf3c079cc17d4a9ecda548a
ogneyar79 <maksimus.s@mail.ru>
2020-10-15 20:26:19 +0300

    CreateAuthServletAndUserEntity

--- src/main/java/servlet/AuthServlet.java
+++ src/main/java/servlet/AuthServlet.java
@@ -1,19 +1,27 @@
 package servlet;
 
+import model.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class AuthServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("AS doPost ");
+        System.out.println("AuthS doPost ");
         String email = req.getParameter("email");
         String password = req.getParameter("password");
         if ("root@local".equals(email) && "root".equals(password)) {
+            HttpSession sc = req.getSession();
+            User admin = new User();
+            admin.setName("Admin");
+            admin.setEmail(email);
+            sc.setAttribute("user", admin);
             resp.sendRedirect(req.getContextPath() + "/posts.do");
         } else {
             req.setAttribute("error", "Не верный email или пароль");

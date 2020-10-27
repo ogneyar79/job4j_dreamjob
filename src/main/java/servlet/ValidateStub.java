bb57bbe66eef5c26f5344e98c7ed621f797d7e2f
ogneyar79 <maksimus.s@mail.ru>
2020-10-27 15:16:13 +0300

    CreateValidateStab class
    That class implements Ivalidate for getting data from  imitation base.

---
+++ src/main/java/servlet/ValidateStub.java
@@ -0,0 +1,31 @@
+package servlet;
+
+import model.User;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ValidateStub implements IValidate {
+
+    private final Map<Integer, User> store = new HashMap<>();
+    private int ids = 0;
+
+    public static IValidate getValidateObject() {
+        return ValidateService.getInstance();
+    }
+
+    @Override
+    public User add(User user) {
+        user.setId(this.ids++);
+        this.store.put(user.getId(), user);
+        return user;
+    }
+
+    @Override
+    public List<User> getAll() {
+        return new ArrayList<User>(this.store.values());
+    }
+}

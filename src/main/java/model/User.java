1b608203137015b5609ae81886c242f8c2b88cca
ogneyar79 <maksimus.s@mail.ru>
2020-10-27 15:20:10 +0300

    AddAt User setter

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -23,34 +23,34 @@ public class User {
         return id;
     }
 
-//    public void setId(int id) {
-//        this.id = id;
-//    }
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
     }
 
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getEmail() {
         return email;
     }
 
-//    public void setEmail(String email) {
-//        this.email = email;
-//    }
-//
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getPassword() {
         return password;
     }
 
-//    public void setPassword(String password) {
-//        this.password = password;
-//    }
-//
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;

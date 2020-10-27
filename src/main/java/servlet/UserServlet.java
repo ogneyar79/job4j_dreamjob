c035b5b8883fa10e5210aa55e0f8fee4994f2d3f
ogneyar79 <maksimus.s@mail.ru>
2020-10-27 15:10:06 +0300

    CreateUserServlet

---
+++ src/main/java/servlet/UserServlet.java
@@ -0,0 +1,23 @@
+package servlet;
+
+import model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class UserServlet extends HttpServlet {
+
+    private final IValidate logic = ValidateStub.getValidateObject();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.logic.add(new User(0, "Bill", "er", "54fg"));
+    }
+}

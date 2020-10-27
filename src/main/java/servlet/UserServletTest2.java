4ecd9dc17be1db82e8ad80052556716a82c07703
ogneyar79 <maksimus.s@mail.ru>
2020-10-27 15:23:15 +0300

    Begin testMockitoAndPowerMockForValidateStab
    Test getting data from ValidateStab using static methods
    and using for this Mockito object.

---
+++ src/main/java/servlet/UserServletTest2.java
@@ -0,0 +1,40 @@
+package servlet;
+
+import model.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(ValidateService.class)
+public class UserServletTest2 {
+
+    @Test
+    public void whenAddUserThenStoreIt() throws ServletException, IOException {
+        IValidate model = new ValidateStub();
+        model.add(new User(0, "Petr Arsentev", "", ""));
+        PowerMockito.mockStatic(ValidateService.class);
+        Mockito.when(ValidateService.getInstance()).thenReturn(model);
+        HttpServletRequest req = mock(HttpServletRequest.class);
+        HttpServletResponse resp = mock(HttpServletResponse.class);
+        when(req.getParameter("name")).thenReturn("Petr Arsentev");
+        new UserServlet().doPost(req, resp);
+        String name = req.getParameter("name");
+        assertThat(name, is("Petr Arsentev"));
+        assertThat(ValidateService.getInstance().getAll().iterator().next().getName(), is("Petr Arsentev"));
+    }
+}

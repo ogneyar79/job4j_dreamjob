85dcefd43b9b5444b6d066ff1df54227e1c17530
ogneyar79 <maksimus.s@mail.ru>
2020-10-31 14:40:43 +0300

    Create class CandidateImitation
    Class was created for imitation of work Base data<Candidate>.

---
+++ src/main/java/store/CandidateImitation.java
@@ -0,0 +1,40 @@
+package store;
+
+import model.Candidate;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CandidateImitation implements IPsqlStoreBase<Candidate> {
+
+    private final Map<Integer, Candidate> store = new HashMap();
+    private int ids = 1;
+
+    private Candidate add(Candidate candidate) {
+        Candidate candidateR = new Candidate(ids, candidate.getName(), candidate.getPhotoId());
+        ids++;
+        this.store.put(candidateR.getId(), candidateR);
+        return candidate;
+    }
+
+    @Override
+    public Collection<Candidate> findAllEntity() {
+        return new ArrayList(this.store.values());
+    }
+
+    @Override
+    public void save(Candidate candidate) {
+        if (candidate.getId() == 0) {
+            add(candidate);
+        } else {
+            this.store.put(candidate.getId(), candidate);
+        }
+    }
+
+    @Override
+    public Candidate findById(int id) {
+        return store.get(id) != null ? store.get(id) : new Candidate(0, "Zero", 000);
+    }
+}
